841f62a69f7be9ca8d5351c6da4d5a749d33b082
Oliver Rietmann <oliver.rietmann@cern.ch>
2025-04-15 16:37:55 +0200

    Fix wrong boost installation

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,6 @@ WORKDIR /root
 
 COPY install.sh /root
 
-RUN bash /root/install.sh && rm /root/install.sh
+RUN bash /root/install.sh && rm -f /root/install.sh /root/anaconda-ks.cfg /root/original-ks.cfg
 
 CMD ["tail", "-f", "/dev/null"]
--- install.sh
+++ install.sh
@@ -3,14 +3,17 @@
 dnf install -y 'dnf-command(config-manager)'
 dnf config-manager --set-enabled crb
 dnf install -y epel-release
-dnf install -y git clang gcc g++ cmake google-benchmark-devel eigen3-devel
+dnf install -y clang gcc g++ cmake google-benchmark-devel eigen3-devel wget
+dnf install -y git # not strictly needed
 
 # install boost-wave from source to get devel files
-git clone --depth 1 --branch boost-1.88.0 https://github.com/boostorg/boost.git
-cd boost
+wget https://archives.boost.io/release/1.82.0/source/boost_1_82_0.tar.gz
+tar -xzf boost_1_82_0.tar.gz
+rm boost_1_82_0.tar.gz
+cd boost_1_82_0
 ./bootstrap.sh --with-libraries=wave
 ./b2
 ./b2 install
 cd ..
-rm -rf boost
+rm -rf boost_1_82_0
 
