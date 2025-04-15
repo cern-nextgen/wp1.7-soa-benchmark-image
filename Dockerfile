fce485d27e9dc80bf246f5e71ccff8b699a0239e
Oliver Rietmann <oliver.rietmann@cern.ch>
2025-04-15 15:36:38 +0200

    cleanup

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,6 @@ WORKDIR /root
 
 COPY install.sh /root
 
-RUN bash /root/install.sh
+RUN bash /root/install.sh && rm /root/install.sh
 
 CMD ["tail", "-f", "/dev/null"]
--- install.sh
+++ install.sh
@@ -3,16 +3,14 @@
 dnf install -y 'dnf-command(config-manager)'
 dnf config-manager --set-enabled crb
 dnf install -y epel-release
-dnf install -y clang gcc g++ cmake google-benchmark-devel eigen3-devel
+dnf install -y git clang gcc g++ cmake google-benchmark-devel eigen3-devel
 
 # install boost-wave from source to get devel files
-dnf install -y wget
-wget https://archives.boost.io/release/1.82.0/source/boost_1_82_0.tar.gz
-tar -xzf boost_1_82_0.tar.gz
-cd boost_1_82_0
+git clone --depth 1 --branch boost-1.88.0 https://github.com/boostorg/boost.git
+cd boost
 ./bootstrap.sh --with-libraries=wave
 ./b2
 ./b2 install
 cd ..
-rm -rf boost_1_82_0 boost_1_82_0.tar.gz
+rm -rf boost
 
