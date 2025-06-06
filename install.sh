f73fa1cd9652553fb37ce8f8d40ad3f113bf52f3
leobeltra <leonardobeltrame00@gmail.com>
2025-06-06 18:29:33 +0200

    Added installation of VTune

--- install.sh
+++ install.sh
@@ -27,6 +27,17 @@ cd boost_1_82_0
 cd ..
 rm -rf boost_1_82_0
 
+# install Intel VTune Profiler
+VTUNE_VERSION="2024.1.0"
+VTUNE_URL="https://registrationcenter-download.intel.com/akdlm/IRC_NAS/8fd43b33-e2fa-4ecf-96b3-3f927c9eb05c/l_oneapi_vtune_p_${VTUNE_VERSION}.offline.sh"
+
+wget ${VTUNE_URL} -O vtune_installer.sh
+chmod +x vtune_installer.sh
+./vtune_installer.sh -s -a install --eula accept --components intel.oneapi.vtune --install-dir /opt/intel/oneapi
+# Add VTune to PATH
+echo 'export PATH=/opt/intel/oneapi/vtune/latest/bin64:$PATH' >> /etc/profile.d/vtune.sh
+echo 'export VTUNE_INSTALL_DIR=/opt/intel/oneapi/vtune/latest' >> /etc/profile.d/vtune.sh
+
 # install python libraries
 dnf install -y python3-pip
 pip3 install --upgrade pip
