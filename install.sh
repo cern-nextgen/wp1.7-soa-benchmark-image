3e2969695eb12effbfdacdc2f42058f64681c819
Jolly Chen <jolly.chen@cern.ch>
2026-02-08 19:11:58 +0100

    Fix clang-p2996 install command

--- install.sh
+++ install.sh
@@ -82,10 +82,10 @@ git clone --depth 1 --branch p2996 https://github.com/bloomberg/clang-p2996.git
 cd clang-p2996 
 dnf install -y lld 
 mkdir build 
-cmake -S llvm -B build -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_C_COMPILER=/usr/bin/clang -DLLVM_ENABLE_LLD=on 
-        -DLLVM_BUILD_LLVM_DYLIB=ON -DLLVM_LINK_LLVM_DYLIB=on -DLLVM_APPEND_VC_REV=off -DLLVM_ENABLE_ASSERTIONS=on 
-        -DLLVM_ENABLE_PROJECTS='clang;clang-tools-extra;lld' -DLLVM_ENABLE_RUNTIMES="libc;libcxx;libcxxabi;libunwind"  
-        -DCMAKE_INSTALL_PREFIX=/data/apps/clang-p2996/install -DCMAKE_BUILD_TYPE=Release -DLLVM_BUILD_DOCS=OFF      
+cmake -S llvm -B build -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_C_COMPILER=/usr/bin/clang -DLLVM_ENABLE_LLD=ON \ 
+        -DLLVM_BUILD_LLVM_DYLIB=ON -DLLVM_LINK_LLVM_DYLIB=ON -DLLVM_APPEND_VC_REV=OFF -DLLVM_ENABLE_ASSERTIONS=ON \
+        -DLLVM_ENABLE_PROJECTS='clang;clang-tools-extra;lld' -DLLVM_ENABLE_RUNTIMES="libc;libcxx;libcxxabi;libunwind" \  
+        -DCMAKE_INSTALL_PREFIX=/root/clang-p2996/install -DCMAKE_BUILD_TYPE=Release -DLLVM_BUILD_DOCS=OFF \
         -DLLVM_BUILD_BENCHMARKS=OFF -DLIBCXX_INSTALL_MODULES=ON -DLLDB_ENABLE_PYTHON=0 
 cmake --build build -j $(nproc) 
 cd ../
